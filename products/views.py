9662732e58e334630e854ead8fbf9b804004b10a
jod35 <jodestrevin@gmail.com>
2025-10-15 21:25:54 +0300

    Revert "views in detail"
    This reverts commit dfa2782247c47cd0101d4aa3c5ac325c97d3cb15.

--- products/views.py
+++ products/views.py
@@ -4,63 +4,17 @@ from django.http import HttpRequest, HttpResponse
 from products.forms import ProductCreateForm
 from .data import products
 from .models import Product
-from django.views.generic import ListView, TemplateView, DetailView
-from django.urls import reverse
 # Create your views here.
-
-
-class HomepageView(ListView):
+def homepage(request: HttpRequest) -> HttpResponse:
     page_title = "Product List"
-    template_name = "index.html"
-    model = Product
-    context_object_name = "products"
+    product_list = Product.objects.all()
+    return render(request,'index.html',
+                  {'title': page_title, 'products': product_list})
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["title"] = self.page_title
-        return context
-
-
-class AboutPage(TemplateView):
+def about_page(request: HttpRequest) -> HttpResponse:
     page_title = "About Page"
-    template_name = "about.html"
+    return render(request,'about.html',{'title': page_title})
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = self.page_title
-        return context
-
-
-class ContactPage(TemplateView):
+def contact_page(request: HttpRequest) -> HttpResponse:
     page_title = "Contact Page"
-    template_name = "contact.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = self.page_title
-        return context
-
-
-class ProductDetailView(DetailView):
-    model = Product
-    template_name = "products/product_detail.html"
-    queryset = Product.objects.all()
-    context_object_name = 'product'
-
-class ProductCreateView(TemplateView):
-    template_name = 'products/product_creation_form.html'
-    form_class = ProductCreateForm
-
-    def get_context_data(self, **kwargs):
-        context =  super().get_context_data(**kwargs)
-        context['form'] = self.form_class()
-        return context
-
-    def post(self, request:HttpRequest):
-
-        form = self.form_class(request.POST,request.FILES)
-
-        if form.is_valid():
-            form.save()
-            return redirect(reverse('homepage'))
-        return redirect(reverse('product_create'))
+    return render(request,'contact.html',{'title': page_title})
